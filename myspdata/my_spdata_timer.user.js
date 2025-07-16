3dc461037fd4138948a1a068a4588fe407a27d90
Jan Homann <j.homann@iss-automation.com>
2025-07-16 13:29:42 +0200

    Removed some code

--- myspdata/my_spdata_timer.user.js
+++ myspdata/my_spdata_timer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         my_spdata_timer
 // @namespace    http://tampermonkey.net/
-// @version      2025-07-15
+// @version      2025-07-16
 // @description  How long do I have to stay
 // @author       weaseldev
 // @match        https://myspdata.duerkopp.com/
@@ -25,12 +25,10 @@
 
     function soLangeNoch() {
         let starttime = document.getElementsByClassName("terminalclock")[0].querySelector("li.list-group-item > p:nth-child(2)").innerText.substr(14,5);
-        let startHours = starttime.substr(0, 2);
-        let startMinutes = starttime.substr(3, 2);
 
         let startDate = new Date();
-        startDate.setHours(startHours);
-        startDate.setMinutes(startMinutes);
+        startDate.setHours(starttime.substr(0, 2));
+        startDate.setMinutes(starttime.substr(3, 2));
         startDate.setSeconds(0);
 
         let dow = startDate.getDay()-1;
@@ -40,10 +38,10 @@
         endDate.setMinutes(endDate.getMinutes() + offset);
 
         let timeLeft = (endDate - new Date())/60/60/1000;
-        let negateTime = 1;
+        let negateTime = false;
         if(timeLeft < 0){
             timeLeft = timeLeft*-1;
-            negateTime = -1;
+            negateTime = true;
         }
         let hLeft = Math.floor(timeLeft);
         let mLeft = (60*(timeLeft-hLeft)).toFixed(0)
@@ -58,6 +56,6 @@
             divEle.classList.add("row");
             document.getElementById("timeterminalfront").querySelector(".col-8").appendChild(divEle);
         }
-        divEle.innerHTML = "Anwesend bis " + endDate.toLocaleTimeString() + " (" + hLeft + ":" + mLeft + "h " + (negateTime > 0 ? "verbleibend)" : "drüber)");
+        divEle.innerHTML = "Anwesend bis " + endDate.toLocaleTimeString() + " (" + hLeft + ":" + mLeft + "h " + (!negateTime ? "verbleibend)" : "drüber)");
     }
 })();
